1397dd05807834d56dc4b327bf311250f0bbf576
Hyndavi <150248106+hyndavi9214@users.noreply.github.com>
2025-03-17 08:39:05 +0530

    Add files via upload

---
+++ EncryptionDecryptionDemo.java
@@ -0,0 +1,17 @@
+package assignments;
+
+class Encryption{
+    String message;
+    String decodeMessage(String message){
+       String Strresult=message.replaceAll("[AEIOUaeiou]","");
+       return  Strresult;
+
+    }
+}
+public class EncryptionDecryptionDemo {
+    public static void main(String[] args) {
+        Encryption e=new Encryption();
+        System.out.println(e.decodeMessage("helloraja"));
+    }
+
+}
